16c3dc46e5bc3b42ae22c9455d8b3602de9de6df
unknown <kothamnaideep@123gmail.com>
2021-07-21 10:42:06 +0530

    lists

---
+++ employees.py
@@ -0,0 +1,9 @@
+employees=["mani","vamsi","guru","rishi"]
+employeeid=[11,12,13,14]
+print(len(employees))
+print(len(employeeid))
+print(employees+employeeid)
+del employees[2]
+employees[2]="sam"
+employeeid.remove(13)
+print(employees+employeeid)
---
+++ movies.py
@@ -0,0 +1,5 @@
+movies=["kushi","badri","gabbarsingh","magadheera"]
+del movies[1]
+print(len(movies))
+movies[1]="rrr"
+print(movies)
